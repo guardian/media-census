a2129f6fb54accb69d3ddf816adadb16b7bd0e62
Andy Gallagher <andy.gallagher@theguardian.com>
2019-06-04 11:25:13 +0100

    initial UI work

---
+++ frontend/app/StatsHistoryGraph.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import LoadingThrobber from "./common/LoadingThrobber.jsx";
+import ErrorViewComponent from "./common/ErrorViewComponent.jsx";
+import {Scatter} from 'react-chartjs-2';
+import RefreshButton from "./common/RefreshButton.jsx";
+import moment from 'moment';
+
+class StatsHistoryGraph extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loading: false,
+            lastError: null,
+            startingTimestamp: null,
+            finishingTimestamp: null,
+            facetData: []
+        }
+    }
+
+    static makeColourValues(count, offset){
+        let values = [];
+        for(let n=0;n<count;++n){
+            let hue = (n/count)*360.0 + offset;
+            values[n] = 'hsla(' + hue + ',75%,50%,0.6)'
+        }
+        return values;
+    }
+
+    componentWillMount() {
+        this.reload();
+    }
+
+    objectToQueryString(params) {
+        if(!params || Object.keys(params).length===0) return "";
+        const joinedString = Object.keys(params).map(k=>k + "=" + params[k]).join("&");
+        return "?" + joinedString;
+    }
+
+    reload(){
+        return new Promise((resolve, reject)=> {
+            let urlParams = {};
+            if (this.state.startingTimestamp) urlParams.startAt = this.state.startingTimestamp;
+            if (this.state.finishingTimestamp) urlParams.endAt = this.state.finishingTimestamp;
+
+            const url = "/api/jobs/forTimespan" + this.objectToQueryString(urlParams);
+
+            this.setState({loading: true, lastError: null}, () => axios.get(url).then(result => {
+                console.log(result.data);
+                const facetData = result.data.entries.map(entry=>{return {
+                    label: entry.scanStart,
+                    epoch: moment(entry.scanStart).unix(),
+                    noBackupsCount: entry.noBackupsCount,
+                    partialBackupsCount: entry.partialBackupsCount,
+                    fullBackupsCount: entry.fullBackupsCount
+                }});
+
+                this.setState({
+                    loading: false,
+                    lastError: null,
+                    facetData: facetData,
+                }, resolve());
+            }).catch(err => {
+                console.error(err);
+                this.setState({loading: false, lastError: err}, reject(err));
+            }))
+        });
+    }
+
+    render(){
+        return <div>
+            <span className="controls-banner"><RefreshButton isRunning={this.state.loading} clickedCb={()=>this.reload()}/></span>
+            <Scatter
+                data={{
+                        labels: this.state.facetData.map(entry=>entry.label),
+                        datasets: [{
+                            label: "No backups",
+                            backgroundColor: "#FF0000",
+                            showLine: true,
+                            data: this.state.facetData.map(entry=>{return {x: entry.epoch, y: entry.noBackupsCount}})
+                        },{
+                            label: "Partial backups",
+                            backgroundColor: "#0000FF",
+                            showLine: true,
+                            data: this.state.facetData.map(entry=>{return {x: entry.epoch, y: entry.partialBackupsCount}})
+                        }, {
+                            label: "Full backups",
+                            backgroundColor: "#00FF00",
+                            showLine: true,
+                            data: this.state.facetData.map(entry=>{return {x: entry.epoch, y: entry.fullBackupsCount}})
+                        }]
+                }}
+                options={{
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "File count",
+                            }
+                        }],
+                        xAxes: [{
+                            labelString: "Date",
+                            ticks: {
+                                callback: (value,index,series)=>moment(value).format("dd Do MMM HH:mm:ss")
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: true,
+                        position: "bottom"
+                    }
+                }}
+            />
+        </div>
+    }
+}
+
+export default StatsHistoryGraph;
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter, Link, Route, Switch, Redirect} from 'react-router-dom';
 import Raven from 'raven-js';
+import StatsHistoryGraph from "./StatsHistoryGraph.jsx";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel, faCheckCircle, faCheck, faTimes, faTimesCircle, faRoad, faSearch,faThList,faWrench, faLightbulb, faFolderPlus, faFolderMinus, faFolder, faBookReader, faRedoAlt, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faChevronCircleDown,faChevronCircleRight,faTrashAlt, faFilm, faVolumeUp,faImage, faFile, faClock, faRunning, faExclamationTriangle, faHdd, faBalanceScale, faSyncAlt, faIndustry, faListOl} from '@fortawesome/free-solid-svg-icons'
+import { faCompressArrowsAlt, faBug, faExclamation, faUnlink } from '@fortawesome/free-solid-svg-icons'
+library.add(faStroopwafel, faCheckCircle, faCheck, faTimes, faTimesCircle, faRoad,faSearch,faThList,faWrench, faLightbulb, faChevronCircleDown, faChevronCircleRight, faTrashAlt, faFolderPlus, faFolderMinus, faFolder);
+library.add(faFilm, faVolumeUp, faImage, faFile, faClock, faRunning, faExclamationTriangle, faHdd, faBalanceScale, faSyncAlt, faBookReader, faBug, faCompressArrowsAlt, faIndustry, faRedoAlt, faHome, faListOl,);
+library.add(faExclamation, faUnlink);
 
 class App extends React.Component {
     render(){
-        return <div><h1>Hello world</h1></div>
+        return <div>
+            <h1>Media Census</h1>
+            <StatsHistoryGraph/>
+        </div>
     }
 }
 
