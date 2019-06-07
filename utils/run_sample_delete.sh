ee70bc25aac1ea823659b5d5bf29e573a00e6307
Andy Gallagher <andy.gallagher@theguardian.com>
2019-06-07 10:08:41 +0100

    removing files that shouldn't be there

--- utils/run_sample_delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --rm -e ASSETSWEEPER_JDBC_URL=jdbc:postgresql://dc1-mmdevdb-02.dc1.gnm.int/asset_folder_importer \
-                -e ASSETSWEEPER_JDBC_USER=testuser \
-                -e ASSETSWEEPER_PASSWORD=testpassword \
-                --network=mediacensus-dev \
-                guardianmultimedia/mediacensus-delscanner:DEV
