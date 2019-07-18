2e8c900f3dd01319f8f8f52495c2a9b4e0308dfc
Andy Gallagher <andy.gallagher@theguardian.com>
2019-07-18 15:52:55 +0100

    VSItemSearchSource now retries on any VS error

--- common/src/main/scala/streamComponents/VSItemSearchSource.scala
+++ common/src/main/scala/streamComponents/VSItemSearchSource.scala
@@ -2,7 +2,7 @@ package streamComponents
 
 import akka.actor.ActorSystem
 import akka.stream.{Attributes, Materializer, Outlet, SourceShape}
-import akka.stream.stage.{AbstractOutHandler, GraphStage, GraphStageLogic}
+import akka.stream.stage.{AbstractOutHandler, AsyncCallback, GraphStage, GraphStageLogic}
 import org.slf4j.LoggerFactory
 import vidispine.{VSCommunicator, VSFile, VSLazyItem}
 
@@ -10,13 +10,13 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 import scala.util.{Failure, Success}
 import scala.xml.XML
+import scala.concurrent.duration._
 
 /*
 content – Comma-separated list of the types of content to retrieve, possible values are metadata, uri, shape, poster, thumbnail, access, merged-access, external.
  */
 /**
   *
-  * @param metdataFields
   * @param searchDoc
   * @param includeShape
   * @param pageSize
@@ -24,7 +24,7 @@ content – Comma-separated list of the types of content to retrieve, possible v
   * @param actorSystem
   * @param mat
   */
-class VSItemSearchSource(metadataFields:Seq[String], searchDoc:String, includeShape:Boolean, pageSize:Int=100)
+class VSItemSearchSource(metadataFields:Seq[String], searchDoc:String, includeShape:Boolean, pageSize:Int=100, retryDelay:FiniteDuration=30.seconds)
                         (implicit val comm:VSCommunicator, actorSystem: ActorSystem, mat:Materializer)
   extends GraphStage[SourceShape[VSLazyItem]] {
 
@@ -54,36 +54,49 @@ class VSItemSearchSource(metadataFields:Seq[String], searchDoc:String, includeSh
     private var queue:Seq[VSLazyItem] = Seq()
     private var currentItem = 1 //VS starts enumerating from 1 not 0
 
+    /**
+      * either services the request from the queue or calls out to VS for more data, refills the queue and processes that.
+      * recurses after a 30s delay if an error is returned from VS.
+      * @param failedCb
+      * @param newItemCb
+      * @param completedCb
+      */
+
+    def processPull(failedCb:AsyncCallback[Throwable], newItemCb:AsyncCallback[VSLazyItem], completedCb:AsyncCallback[Unit]):Unit = {
+      if (queue.nonEmpty) {
+        logger.debug(s"Serving next item from queue")
+        push(out, queue.head)
+        queue = queue.tail
+      } else {
+        getNextPage(currentItem).onComplete({
+          case Failure(err) =>
+            logger.error(s"getNextPage crashed", err)
+            failedCb.invoke(err)
+          case Success(Left(httpError)) =>
+            logger.error(s"VS returned an http error ${httpError.errorCode}: ${httpError.message}, retrying in 30 seconds")
+            actorSystem.scheduler.scheduleOnce(retryDelay, () =>processPull(failedCb, newItemCb, completedCb))
+          //failedCb.invoke(new RuntimeException(httpError.message))
+          case Success(Right(moreItems)) =>
+            logger.info(s"Got ${moreItems.length} more items from server, processed $currentItem items")
+            queue ++= moreItems
+            currentItem += moreItems.length
+            if (queue.isEmpty) {
+              completedCb.invoke(())
+            } else {
+              newItemCb.invoke(queue.head)
+              queue = queue.tail
+            }
+        })
+      }
+    }
+
     setHandler(out, new AbstractOutHandler {
       override def onPull(): Unit = {
         val failedCb = createAsyncCallback[Throwable](err=>failStage(err))
         val newItemCb = createAsyncCallback[VSLazyItem](item=>push(out, item))
         val completedCb = createAsyncCallback[Unit](_=>complete(out))
 
-        if(queue.nonEmpty){
-          logger.debug(s"Serving next item from queue")
-          push(out, queue.head)
-          queue = queue.tail
-        } else {
-          getNextPage(currentItem).onComplete({
-            case Failure(err)=>
-              logger.error(s"getNextPage crashed", err)
-              failedCb.invoke(err)
-            case Success(Left(httpError))=>
-              logger.error(s"VS returned an http error ${httpError.errorCode}: ${httpError.message}")
-              failedCb.invoke(new RuntimeException(httpError.message))
-            case Success(Right(moreItems))=>
-              logger.info(s"Got ${moreItems.length} more items from server, processed $currentItem items")
-              queue ++= moreItems
-              currentItem+=moreItems.length
-              if(queue.isEmpty){
-                completedCb.invoke(())
-              } else {
-                newItemCb.invoke(queue.head)
-                queue = queue.tail
-              }
-          })
-        }
+        processPull(failedCb, newItemCb, completedCb)
       }
     })
   }
--- common/src/test/scala/VSItemSearchSourceSpec.scala
+++ common/src/test/scala/VSItemSearchSourceSpec.scala
@@ -1,5 +1,6 @@
 import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink}
 import akka.stream.{ActorMaterializer, ClosedShape, Materializer}
+import models.HttpError
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import streamComponents.VSItemSearchSource
@@ -45,6 +46,32 @@ class VSItemSearchSourceSpec extends Specification with Mockito{
 
       result.head.getSingle(FieldNames.EXTERNAL_ARCHIVE_STATUS) must beSome("Archived")
       //there was one(mockedComm).request("/API/item;first=101;number=100","mock-search-doc",Map("Accept"->"application/xml"),Map("content"->"metadata","field"->"field1,field2"))
+    }
+
+    "retry on error" in new AkkaTestkitSpecs2Support {
+      implicit val mat:Materializer = ActorMaterializer.create(system)
+
+      implicit val mockedComm = mock[VSCommunicator]
+      mockedComm.request(any,any,any,any,any)(any,any) returns Future(Left(HttpError("Vidispine is playing up",503))) thenReturns Future(Right(getTestSearch("testsearch.xml"))) thenReturns Future(Right(getTestSearch("emptysearch.xml")))
+
+      val sinkFactory = Sink.fold[Seq[VSLazyItem],VSLazyItem](Seq())((acc,entry)=>acc++Seq(entry))
+
+      val graph = GraphDSL.create(sinkFactory) { implicit builder=> sink=>
+        import akka.stream.scaladsl.GraphDSL.Implicits._
+
+        val src = builder.add(new VSItemSearchSource(Seq("field1","field2"),"mock-search-doc",true, retryDelay = 1.second))
+        src ~> sink
+        ClosedShape
+      }
+
+      val result = Await.result(RunnableGraph.fromGraph(graph).run(), 30 seconds)
+      println(s"Got ${result.length} items:")
+      result.foreach(item=>println(s"\t$item"))
+      result.length mustEqual 18
+
+      there were three(mockedComm).request("/API/item;first=1;number=100","mock-search-doc",Map("Accept"->"application/xml"),Map("content"->"metadata","field"->"field1,field2"))
+
+      result.head.getSingle(FieldNames.EXTERNAL_ARCHIVE_STATUS) must beSome("Archived")
     }
   }
 }
